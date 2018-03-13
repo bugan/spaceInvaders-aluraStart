32fdfc480f8eca1948dc948b4b6f49c0e5cbf2e7
Ricardo <dones.bugan@gmail.com>
2018-03-13 12:26:11 -0300

    Aula 1 vídeo 4

--- jogo/sketch.js
+++ jogo/sketch.js
@@ -1,31 +1,52 @@
 let imagemNave;
 let imagemAlien;
+let imagemMissil;
 
-let deslocamentoAlienX;
+let posicaoNave;
+let posicaoInicialAlien;
+let posicaoMissil;
+
+let deslocamentoAlien;
 let limiteHorizontalAlien;
+
 let velocidadeHorizontalAlien;
+let velocicadeMissil;
+
 
 function preload() {
     imagemNave = loadImage("imagens/Nave.png");
     imagemAlien = loadImage("imagens/Alien1.png");
+    imagemMissil = loadImage("imagens/Missil.png");
 }
 
 function setup() {
     createCanvas(900, 600);
-    deslocamentoAlienX = 0;
+    deslocamentoAlien = createVector(0,0);
+    
+    posicaoInicialAlien = createVector(0,100);
+    posicaoNave = createVector(0, 500);
+    posicaoMissil = createVector(400, 600);
+
     limiteHorizontalAlien = 750;
-    velocidadeHorizontalAlien = 1
+    velocidadeHorizontalAlien = 1;
+    velocicadeMissil = 3;
+    
 }
 
 function draw() {
         background(100);
 
-        deslocamentoAlienX += velocidadeHorizontalAlien;
-        if(deslocamentoAlienX % limiteHorizontalAlien == 0){
+        deslocamentoAlien.x += velocidadeHorizontalAlien;
+        if(deslocamentoAlien.x % limiteHorizontalAlien == 0){
             velocidadeHorizontalAlien *= -1;
         }
 
-        image(imagemNave, mouseX, 500);
-        image(imagemAlien, deslocamentoAlienX, 100);
+        posicaoNave.x = mouseX;
+        posicaoMissil.y -= velocicadeMissil;
+        
+
+        image(imagemMissil, posicaoMissil.x, posicaoMissil.y);
+        image(imagemNave, posicaoNave.x, posicaoNave.y);
+        image(imagemAlien, posicaoInicialAlien.x + deslocamentoAlien.x, posicaoInicialAlien.y);
 }
 
